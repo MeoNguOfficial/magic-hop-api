a00906b12cd6de3d3112414b9bd2a9c58b704cd1
Trần Quốc Khánh <meongutw@gmail.com>
2026-08-05 08:44:47 +0700

    fix lỗi add users admin

--- routes/api.php
+++ routes/api.php
@@ -50,6 +50,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
     Route::get('/users', [UserController::class, 'index']);
+    Route::post('/users', [UserController::class, 'store']);
     Route::get('/users/{id}', [UserController::class, 'show']);
     Route::put('/users/{id}', [UserController::class, 'update']);
     Route::delete('/users/{id}', [UserController::class, 'destroy']);
